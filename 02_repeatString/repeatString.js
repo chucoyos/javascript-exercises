a025d8a326c85e1a6990e1b351582b152e78cbff
unodev <unodev@outlook.com>
2023-01-25 23:30:21 -0600

    Add repeatString test suite

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let myStr = "";
+  if(num < 0){
+    return "ERROR";
+  }
 
+  for(let i = 0; i < num; i++){
+    myStr += str;
+  }
+  return myStr;
 };
 
 // Do not edit below this line
