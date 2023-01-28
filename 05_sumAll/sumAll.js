cfdb9d6877a36a906a9c1be53deed099325d7226
chucoyos <unodev@outlook.com>
2023-01-28 10:09:48 -0600

    Add sumAll test suite

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+  let sum = 0;
+  if(a < 0 || b < 0){
+    return "ERROR"
+  } else if(isNaN(a) || isNaN(b)){
+    return "ERROR"
+  } else if(typeof(a) === "string" || typeof(b) === "string"){
+    return "ERROR"
+  } else
+  if(a < b){
+      for(let i = a; i <= b; i++){
+        sum += i;
+    }
+  } else if(b < a){
+    for(let i = b; i <= a; i++){
+      sum += i
+    }
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
