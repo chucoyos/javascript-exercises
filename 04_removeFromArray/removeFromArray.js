7c77f6a429e0997951ea49a029b9da8be6f1d808
unodev <unodev@outlook.com>
2023-01-27 23:09:07 -0600

    Add removeFromArray test suite

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(ar, ...args) {
+  // for (let i = 0; i < args.length; i++) {
+  //   let index = ar.indexOf(args[i]);// find the index of the element
+  //   if (index > -1) { // if the element is in the array
+  //     ar.splice(index, 1); // remove it
+  //   }
+  // }
 
+  // return ar;
+  return ar.filter((item) => !args.includes(item))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
