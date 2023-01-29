6340d4d7cdd84c7e378d14e7fd6f0e344b5d3eb6
unodev <unodev@outlook.com>
2023-01-29 00:45:02 -0600

    Pass tempConversion test suite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) / 1.8000;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = c * 1.8000 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
