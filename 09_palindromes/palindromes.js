6ef532a4f2a6613fc3d70a249e82d54ecbb54a27
unodev <unodev@outlook.com>
2023-01-29 22:38:51 -0600

    Pass palindromes test suite

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  var str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  var str2 = str.split('').reverse().join('');
+  if (str === str2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
