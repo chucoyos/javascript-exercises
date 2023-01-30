60e14ae4d6634d7f6652486bb8c4772cc08dff1e
unodev <unodev@outlook.com>
2023-01-29 18:55:29 -0600

    Pass calculator test suite

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...num) {
+  let sum = num.reduce((acc, current) => acc + current)
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...sub) {
+	let result = sub.reduce((previous, current) => previous - current);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+  let initialValue = 0;
+	let totalSum = ar.reduce((previous, current) => previous + current, initialValue)
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  let total = ar.reduce((previous, current) => previous * current)
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exp ) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+   
+  if(num === 0){
+    return 1;
+  } else
+  for(let i = num;i > 1; i--){
+     result *= (i -1);
+  }
+  return result;
 };
 
 // Do not edit below this line
