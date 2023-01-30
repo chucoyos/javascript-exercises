f99491551ee847651ba24893453e29b6c20ed641
unodev <unodev@outlook.com>
2023-01-30 00:29:01 -0600

    Pass fibonacci test suite

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+  let fibArray = [1, 1]; // 1,1,2,3,5,8,13
 
+  if(fib < 3){
+    if(fib < 0){
+      return "OOPS"
+    }
+    return 1;
+  } else{
+    for(let i = 3; i <= fib; i++){
+      let nextItem = fibArray[i - 2] + fibArray[i - 3]
+      fibArray.push(nextItem);
+    }
+  }
+  return fibArray[fibArray.length -1];
 };
 
 // Do not edit below this line
