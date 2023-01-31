b806fd84cd8173df5f82facb1d6f674ba0145cd8
unodev <unodev@outlook.com>
2023-01-30 22:00:59 -0600

    Pass getTheTitles test suite

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(ar) {
+  let titlesArray = [];
+  for(let i = 0; i < ar.length; i ++){
+    titlesArray.push(ar[i].title)
+  }
+  return titlesArray;
 };
 
 // Do not edit below this line
