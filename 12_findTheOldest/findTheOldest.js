74c75180b9ba943ef31e61a9c0e3814c4da56609
unodev <unodev@outlook.com>
2023-02-05 15:28:20 -0600

    Pass findTheOldest test suite

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ar) {
+  let oldest = ar[0];
+  for(let i = 0; i < ar.length; i ++){
+    if(ar[i].yearOfDeath === undefined){
+      ar[i].yearOfDeath = new Date().getFullYear();
+    }
+    let age = ar[i].yearOfDeath - ar[i].yearOfBirth;
+    if(age > oldest.yearOfDeath - oldest.yearOfBirth){
+      oldest = ar[i];
+    }
+  }
+  return oldest;
+   
 };
 
 // Do not edit below this line
